e16df3cf1cb38ed45538151007dd045d90c3e94b
Noah Dumba <noahdumba1@gmail.com>
2020-01-15 20:00:02 +0000

    moved the connect function to its own file

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.168.104',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-}
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
